32c4a78cd9b846983a42fa1927eeff65386e3c51
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-04 22:29:55 +0800

    Delete backend directory

--- backend/todoapp/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import items
-
-class itemsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = items
-        fields = ['id', 'title', 'completed']
