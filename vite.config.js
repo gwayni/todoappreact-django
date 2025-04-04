5be4cc2337441a4c42c4aa23ea3844b4d12ff0b3
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-04 22:55:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gwayni.github.io/todoappreact-django/",
+  base: "/todoappreact-django/",
 })
