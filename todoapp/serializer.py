54c186d702cde5b0540daf0ca0339930a59501a9
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-04 22:09:39 +0800

    Add files via upload

---
+++ todoapp/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import items
+
+class itemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = items
+        fields = ['id', 'title', 'completed']
