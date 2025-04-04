93e7e76a4a8a5f0743d204baf23dde0ac72cfe89
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-05 00:27:34 +0800

    Update TodoList.jsx

--- frontend/src/TodoList.jsx
+++ frontend/src/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 
-const API_URL = "https://todoappreact-django.onrender.com";
+const API_URL = "https://todoappreact-django.onrender.com/api/list/";
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([]);
